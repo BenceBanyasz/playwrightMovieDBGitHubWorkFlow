67afd7d46c4320ca6735d2d6510cba2d2d05d2f5
Bence Banyasz <Bence_Banyasz@epam.com>
2023-10-13 13:59:05 +0200

    replaced setup with globalconfig

--- global-setup.ts
+++ global-setup.ts
@@ -1,11 +1,12 @@
 require("dotenv").config();
-import { test as setup } from "../fixtures/fixtures";
-import { getSecrets } from "../authentication/retrieve-aws-creds-ci";
+import { getSecrets } from "./tests/authentication/retrieve-aws-creds-ci";
 
-setup("get secrets", async () => {
+async function globalSetup(){
     const { username, password, moviedb_access_token } = await getSecrets();
 
     process.env.MOVIEDB_USERNAME = process.env.MOVIEDB_USERNAME || username;
     process.env.MOVIEDB_PASSWORD = process.env.MOVIEDB_PASSWORD || password;
     process.env.MOVIEDB_ACCESS_TOKEN = process.env.MOVIEDB_ACCESS_TOKEN || moviedb_access_token;
-});
+}
+
+export default globalSetup;
