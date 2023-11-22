164ee5dffa72553cc9b5d794637cfa823a3b6413
Bence Banyasz <Bence_Banyasz@epam.com>
2023-11-22 15:59:11 +0100

    github actions setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
     //Number of retries upon failing tests
     retries: 2,
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    globalSetup: require.resolve('./global-setup'),
     use: {
         headless: true,
         /* Base URL to use in actions like `await page.goto('/')`. */
